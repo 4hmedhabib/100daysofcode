8592d1d8ddd2d8f755e94cf931b44885df9e3b2f
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-01-15 16:29:20 +0300

    updated layout

--- 01 - Comments-Section/code/components/Card/index.jsx
+++ 01 - Comments-Section/code/components/Card/index.jsx
@@ -1,6 +1,9 @@
 const Card = ({ children }) => {
   return (
-    <div className="shadow-md shadow-lightGray bg-white rounded-md px-7 py-4 lg:w-2/3 flex items-center">
+    <div
+      id="comment"
+      className="shadow-md shadow-lightGray bg-white rounded-md px-7 py-4 lg:w-2/3 flex items-center"
+    >
       {children}
     </div>
   );
