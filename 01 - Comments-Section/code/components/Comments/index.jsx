2b7d46233470a9c9d44fd587faada3f2518b5b92
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-01-16 17:35:43 +0300

    added some responsive features

--- 01 - Comments-Section/code/components/Comments/index.jsx
+++ 01 - Comments-Section/code/components/Comments/index.jsx
@@ -1,87 +1,12 @@
 import { Fragment } from "react";
-import { Card, Counter, Content, Reply } from "../../components";
+import { Card, Counter, Comment, Reply } from "../../components";
 
-const Comments = () => {
-  const data = {
-    currentUser: {
-      image: {
-        png: "/avatars/image-juliusomo.png",
-        webp: "/avatars/image-juliusomo.webp",
-      },
-      username: "juliusomo",
-    },
-    comments: [
-      {
-        id: 1,
-        content:
-          "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-        createdAt: "1 month ago",
-        score: 12,
-        user: {
-          image: {
-            png: "/avatars/image-amyrobson.png",
-            webp: "/avatars/image-amyrobson.webp",
-          },
-          username: "amyrobson",
-        },
-        replies: [],
-      },
-      {
-        id: 2,
-        content:
-          "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
-        createdAt: "2 weeks ago",
-        score: 5,
-        user: {
-          image: {
-            png: "/avatars/image-maxblagun.png",
-            webp: "/avatars/image-maxblagun.webp",
-          },
-          username: "maxblagun",
-        },
-        replies: [
-          {
-            id: 3,
-            content:
-              "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
-            createdAt: "1 week ago",
-            score: 4,
-            replyingTo: "maxblagun",
-            user: {
-              image: {
-                png: "/avatars/image-ramsesmiron.png",
-                webp: "/avatars/image-ramsesmiron.webp",
-              },
-              username: "ramsesmiron",
-            },
-          },
-          {
-            id: 4,
-            content:
-              "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
-            createdAt: "2 days ago",
-            score: 2,
-            replyingTo: "ramsesmiron",
-            user: {
-              image: {
-                png: "/avatars/image-juliusomo.png",
-                webp: "/avatars/image-juliusomo.webp",
-              },
-              username: "juliusomo",
-            },
-          },
-        ],
-      },
-    ],
-  };
-
+const Comments = ({ data, currentUser }) => {
   const comments = [];
 
   for (let comment of data.comments) {
     comments.push(comment);
   }
-
-  console.log(comments);
 
   return (
     <Fragment>
@@ -89,14 +14,11 @@ const Comments = () => {
         return (
           <Fragment key={comment.id}>
             <Card>
-              <Counter score={comment.score} />
-              <Content
-                user={comment.user}
-                createdAt={comment.createdAt}
-                content={comment.content}
-              />
+              <Comment />
             </Card>
-            {comment.replies.length > 0 && <Reply replies={comment.replies} />}
+            {comment.replies.length > 0 && (
+              <Reply replies={comment.replies} currentUser={currentUser} />
+            )}
           </Fragment>
         );
       })}
--- 01 - Comments-Section/code/components/index.js
+++ 01 - Comments-Section/code/components/index.js
@@ -1,6 +1,6 @@
 export { default as Card } from "./Card";
 export { default as Counter } from "./Comments/Comment/Counter";
-export { default as Content } from "./Comments/Comment/";
+export { default as Comment } from "./Comments/Comment/";
 export { default as Comments } from "./Comments/";
 export { default as Reply } from "./Comments/Reply/";
 export { default as ReplyCard } from "./Card/ReplyCard";
