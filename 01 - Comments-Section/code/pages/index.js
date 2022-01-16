c11724afd7c540b88128e3a32305201f719c814d
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-01-16 07:56:13 +0300

    added reply section

--- 01 - Comments-Section/code/pages/index.js
+++ 01 - Comments-Section/code/pages/index.js
@@ -2,7 +2,10 @@ import { Comments } from "../components";
 
 export default function Home() {
   return (
-    <div id="container" className="px-2 font-rubik">
+    <div
+      id="container"
+      className="px-2 font-rubik flex gap-3 flex-col items-center"
+    >
       <Comments />
     </div>
   );
